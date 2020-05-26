d7f541b23597d9abb04f81936bbd6728586b0a98
tavor <mf.galesic@gmail.com>
2020-05-26 09:51:12 -0700

    Fix linter issues
    card https://github.com/tavor/maxhotkey/projects/1#card-38894993

--- hotkeys.py
+++ hotkeys.py
@@ -1,42 +1,47 @@
-import configure
+"""
+See README.md
+"""
 import json
 import os
+import configure
 
 from keyboard_map import KeyBoardMapCreator
 from macro.bind import MacroBinderCreator
 from macro.write import MacroWriter
 
-config = configure.get_configuration(os.path.join("resources", "program_configuration.ini"))
-app_data_directory = config['directories']['app_data_directory']
-base_kbdx = config['directories']['base_kbdx']
+PROGRAM_CONFIG = configure.get_configuration(os.path.join("resources", "program_configuration.ini"))
+APP_DATA_DIRECTORY = PROGRAM_CONFIG['directories']['app_data_directory']
+BASE_KBDX = PROGRAM_CONFIG['directories']['base_kbdx']
 
-macro_writer = MacroWriter(
+MACRO_WRTIER = MacroWriter(
     "macros",
-    os.path.join(app_data_directory, "usermacros"),
+    os.path.join(APP_DATA_DIRECTORY, "usermacros"),
     "resources",
     os.path.join("resources", "macro_template"))
-macro_binder = MacroBinderCreator(app_data_directory, base_kbdx)
-keyboard_map = KeyBoardMapCreator(
+MACRO_BINDER = MacroBinderCreator(APP_DATA_DIRECTORY, BASE_KBDX)
+KEYBOARD_MAP = KeyBoardMapCreator(
     os.path.join("resources", "keyboard-layout_blank.png"),
     os.path.join("resources", "keys.cfg"),
     os.path.join("resources", "AcariSans-Regular.ttf"))
 
-hotkey_config = json.load(open(os.path.join("resources", "config.json")))
+HOTKEY_CONFIG = json.load(open(os.path.join("resources", "config.json")))
 
-for keyboard_key in hotkey_config.keys():
+for keyboard_key in HOTKEY_CONFIG.keys():
     for key_combo in ["key", "shift", "ctrl", "alt", "shift-alt", "ctrl-alt"]:
-        if key_combo in hotkey_config[keyboard_key].keys():
-            macro_binder.bind(keyboard_key, key_combo)
-            macro_writer.write(hotkey_config[keyboard_key][key_combo]["macro"], keyboard_key, key_combo)
+        if key_combo in HOTKEY_CONFIG[keyboard_key].keys():
+            MACRO_BINDER.bind(keyboard_key, key_combo)
+            MACRO_WRTIER.write(HOTKEY_CONFIG[keyboard_key][key_combo]["macro"],
+                               keyboard_key, key_combo)
 
-            if keyboard_key.lower() in keyboard_map.get_defined_keys():
-                keyboard_map.add(keyboard_key, hotkey_config[keyboard_key][key_combo]["macro"]["name"])
+            if keyboard_key.lower() in KEYBOARD_MAP.get_defined_keys():
+                KEYBOARD_MAP.add(keyboard_key,
+                                 HOTKEY_CONFIG[keyboard_key][key_combo]["macro"]["name"])
             else:
-                print("Warning: " + hotkey_config[keyboard_key][key_combo]["macro"]["name"] +
+                print("Warning: " + HOTKEY_CONFIG[keyboard_key][key_combo]["macro"]["name"] +
                       " is bound in 3DS Max, but not added to the keyboard map. " +
-                      "Please add an entry for" + keyboard_key + " in " + os.path.join("resources", "keys.cfg") +
-                      " for your macro to show on the map.")
+                      "Please add an entry for" + keyboard_key + " in " +
+                      os.path.join("resources", "keys.cfg") + " for your macro to show on the map.")
 
-keyboard_map.save()
-macro_binder.save_key_bindings()
+KEYBOARD_MAP.save()
+MACRO_BINDER.save_key_bindings()
 
